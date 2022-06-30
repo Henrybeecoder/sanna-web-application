bf88ebe2f8c7b9cf6c4076f9f53788fe87cae9ac
Henrybeecoder <omofonmwanhenrynosa@gmail.com>
2022-06-30 12:57:01 +0100

    new push

---
+++ src/component/AdSpace/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+import Button from "../Button";
+
+function Adspace() {
+  return (
+    <div className="adspace">
+      <div className="content">
+        <p>Get More From Us</p>
+        <Button color="primary">Explore</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Adspace;
--- src/screens/HomePage/index.tsx
+++ src/screens/HomePage/index.tsx
@@ -2,11 +2,113 @@ import React from "react";
 import "./style.css";
 import Header from "../../Layout/Header";
 import Footer from "../../Layout/Footer";
+import likeIcon from "../../Images/likeIcon.svg";
 
 export default function HomePage() {
+  const paintingData = [
+    {
+      id: 1,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655730805/sanna/sanna_img_t6air3.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+    {
+      id: 2,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655737073/sanna/sanna_img2_zhkw9i.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+    {
+      id: 3,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655737197/sanna/sanna_img3_ys1mc7.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+    {
+      id: 4,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655737073/sanna/sanna_img2_zhkw9i.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+    {
+      id: 5,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655737197/sanna/sanna_img3_ys1mc7.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+    {
+      id: 6,
+      imgData:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655730805/sanna/sanna_img_t6air3.svg",
+      name: "African Woman",
+      price: "$5000",
+      userName: "Dianne Russell",
+      userProfession: "Painter",
+      userImg:
+        "https://res.cloudinary.com/henrybee558/image/upload/v1655732127/sanna/Avatar_vgdf4d.svg",
+    },
+  ];
   return (
     <div>
       <Header />
+      <div className="home-page">
+        <h1>Paintings</h1>
+        <div className="container">
+          {paintingData.map((data) => (
+            <div className="parent-div">
+              <div
+                style={{
+                  backgroundImage: `url(${data.imgData}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)`,
+                  backgroundRepeat: "no-repeat",
+                  width: "100%",
+                  height: "416px",
+                  backgroundSize: "cover",
+                }}
+              >
+                <div className="user-container">
+                  <img src={data.userImg} alt="" />
+                  <div className="text-box">
+                    <h3>{data.userName}</h3>
+                    <p>{data.userProfession}</p>
+                  </div>
+                </div>
+              </div>
+              <div className="pricing">
+                <h2>{data.name}</h2>
+                <p>{data.price}</p>
+              </div>
+              <div className="like-icon">
+                <img src={likeIcon} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <Footer />
     </div>
   );
